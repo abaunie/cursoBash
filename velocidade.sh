9c9beb58c4162607bc9bba88462ec271ad7bf5ef
abaunie <128470860+abaunie@users.noreply.github.com>
2023-03-21 23:17:29 +0100

    Update velocidade.sh

--- velocidade.sh
+++ velocidade.sh
@@ -1,16 +1,21 @@
 #/bin/bash
 
-//Comprobamos que recibimos alo menos un parámetro
+#Comprobamos que recibimos alo menos un parámetro
 if [[ $# -ne 0 ]]; then
-  //comprobamos que o primeiro parámetro sea c ou s
+  #comprobamos que o primeiro parámetro sea c ou s
   if [[ $1 == "c" || $1 == "s" ]]; then
-    //comprobamos que o segundo parámetros sexa un parámetro pertencente ao sistema
+    #comprobamos que o segundo parámetros sexa un parámetro pertencente ao sistema
     if [[ -f $2 ]]; then
-      echo "Habemus espacio"
       if [[ $1 == "c" ]]; then
-        speedtest-cli
+        speedtest-cli  | awk '$1 ~ /^Download|^Upload/' | awk '{print $2}' > $2
       elif [[ $1 == "s" ]]; then 
-        speedtest-cli --simple | awk '{$2}' >> $2
+        speedtest-cli --simple | awk '{print $2}' | tail -2 > $2
+      fi
+      #hai valores dentro do ficheiro
+      if [[ `tail -1 $2` > 3.0 ]]; then
+        echo "a tua conexion e rapida"
+      else
+	      echo "a tua conexion e lenta"
       fi
     else
       echo "Entrada incorrecta, debe ser coma segue: ./velocidade.sh c nombredelarchivo.extension"
